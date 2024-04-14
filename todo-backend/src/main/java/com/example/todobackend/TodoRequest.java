8f652175546469912de5ab73301822c5ac22038a
Masatoshi Tada <rosso.e.nero84@gmail.com>
2024-04-14 13:01:31 +0900

    コンパイルエラー修正

--- todo-backend/src/main/java/com/example/todobackend/TodoRequest.java
+++ todo-backend/src/main/java/com/example/todobackend/TodoRequest.java
@@ -3,6 +3,6 @@ package com.example.todobackend;
 public record TodoRequest(String description) {
 
     public Todo toEntity() {
-        return new Todo(null, description);
+        return new Todo(null, description, null);
     }
 }
